15f3513a4acacf353bee62c9114ff87d1dbd874f
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-27 16:51:25 +0200

    Add Categories controllers

---
+++ controllers/CategoryController.js
@@ -0,0 +1,35 @@
+const asyncHandler = require("express-async-handler");
+const Category = require("../models/Category");
+
+// @desc    Get All Categories
+// @route   GET /api/v1/categories
+// @access  Public
+module.exports.getCategories = asyncHandler(async (req, res, next) => {
+  const categories = Category.find().select("-posts");
+  res.status(200).json({ categories });
+});
+
+// @desc    Create new category
+// @route   POST /api/v1/categories
+// @access  Private
+module.exports.createCategegory = asyncHandler(async (req, res, next) => {
+  const { name } = req.body;
+  await Category.create({
+    name,
+  });
+  res.status(200).json({ success: "Category created successfully" });
+});
+
+// @desc    Delete Category by name
+// @route   DELETE /api/v1/categories/:name
+// @access  Private
+module.exports.getCategories = asyncHandler(async (req, res, next) => {
+  const name = req.params.name;
+  const category = Category.find({ name });
+  if (!category) {
+    res.status(404);
+    throw new Error("Category not found");
+  }
+  await Category.deleteOne({ name });
+  res.status(200).json({ success: "Category deleted successfully" });
+});
--- models/Category.js
+++ models/Category.js
@@ -4,6 +4,7 @@ const CategorySchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   },
   posts: [
     {
