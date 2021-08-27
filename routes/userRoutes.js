088c6af4d6f4e8a0e9feb1f0efcaa7d43a238bfe
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-27 15:31:03 +0200

    change the older of the user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,18 +13,22 @@ const { admin, protect } = require("../middleware/authMiddleware");
 
 const userRouter = express.Router();
 
-userRouter.route("/login").post(authUser);
 userRouter
   .route("/")
   .post(registerUser)
-  .get(admin, getUsers)
-  .delete(admin, deleteUser);
-userRouter.route("/:id").get(admin, getUserById).put(admin, updateUser);
+  .get(protect, admin, getUsers)
+  .delete(protect, admin, deleteUser);
+
+userRouter.route("/login").post(authUser);
 
 userRouter
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 
-userRouter.route("/");
+userRouter
+  .route("/:id")
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
+
 module.exports = userRouter;
