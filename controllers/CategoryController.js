d2653a8c91b1c2533507be3c3924f3c153ed025f
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-27 16:55:40 +0200

    Create the updateCategory controller

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -9,6 +9,26 @@ module.exports.getCategories = asyncHandler(async (req, res, next) => {
   res.status(200).json({ categories });
 });
 
+// @desc    Update Category
+// @route   PUT /api/v1/categories/:name
+// @access  Private
+module.exports.updateCategory = asyncHandler(async (req, res, next) => {
+  const name = req.params.name;
+  if (!req.body.name) {
+    res.status(400);
+    throw new Error("New name is required");
+  }
+  const category = Category.find({ name });
+  if (!category) {
+    res.status(404);
+    throw new Error("Category not found");
+  }
+
+  category.name = req.body.name;
+  category.save();
+  res.status(200).json({ success: "Category updated successfully" });
+});
+
 // @desc    Create new category
 // @route   POST /api/v1/categories
 // @access  Private
