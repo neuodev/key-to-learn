c0b26f0e7879d18bba308304a04c480b2da217b3
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-27 20:09:11 +0200

    Create the Message model

---
+++ models/Message.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", MessageSchema);
+module.exports = Message;
