d3013c615d1c4850f026e7a4cd679e52a43e4af7
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-29 15:06:17 +0200

    Restun only publised posts

--- client/pages/admin/index.js
+++ client/pages/admin/index.js
@@ -1,7 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/Admin/Sidebar";
-
+import { useSelector } from "react-redux";
+import { useRouter } from "next/dist/client/router";
 const Admin = () => {
+  const [allPosts, setAllPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState({
+    type: "",
+    message: "",
+  });
+  // @todo redirect to login if it is not and admin
+  const user = useSelector((state) => state.user);
+  const router = useRouter();
+  useEffect(() => {
+    if (!user || !user.userInfo || !user.userInfo.isAdmin) {
+      router.push("/sign-in");
+    }
+  });
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const { data } = await axios.get("/api/v1/posts", {
+        params: {
+          select: "domain,header,slug,likes,createdAt,updatedAt,published",
+        },
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${user.userInfo.token}`,
+        },
+      });
+      console.log(data);
+    };
+
+    fetchPosts();
+  }, []);
   return (
     <div className="flex w-full h-full  min-h-screen">
       <div className="">
