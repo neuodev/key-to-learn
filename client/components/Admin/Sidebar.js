5165c44f2414088fdd46d14f324d6b2a67b10e21
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-29 16:31:16 +0200

    Update Admin sidebar

--- client/components/Admin/Sidebar.js
+++ client/components/Admin/Sidebar.js
@@ -7,33 +7,33 @@ const join = "Join";
 
 const MENU_ITEMS = [
   {
-    title: "Create Post",
-    path: "/admin/posts/create",
-  },
-  {
-    title: "Update Post",
-    path: "/admin/posts/update",
-  },
-  {
-    title: "All Posts",
+    title: "Posts",
     path: "/admin/",
+  },
+  {
+    title: "Categories & Tags",
+    path: "/admin/categories",
+  },
+  {
+    title: "Comments",
+    path: "/admin/comments",
   },
 ];
 
 const SideBar = ({}) => {
   return (
     <div id="admin-page-sidebar" className={`w-64 shadow-xl bg-white h-screen`}>
-      <div className="w-full h-full p-4">
+      <div className="w-full h-full py-4">
         <div className="my-4">
           <ul>
             {MENU_ITEMS.map((tab) => (
               <li
                 key={tab.title}
-                className="mb-1 font-medium text-lg text-center "
+                className="mb-1 font-thin text-lg text-left px-2 "
               >
                 <Link href={tab.path}>
                   <p
-                    className={` py-3 w-full inline-block cursor-pointer rounded-md ${
+                    className={` py-3 px-4 w-full inline-block cursor-pointer rounded-md ${
                       tab.title === join
                         ? "bg-blue-300 text-blue-900 hover:bg-blue-200"
                         : "hover:bg-gray-100"
