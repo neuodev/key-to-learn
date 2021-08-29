1674442b6a283a12ed6ef476068d7ffa7f42edd5
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-29 16:38:18 +0200

    Add new post button

--- client/components/Admin/posts/PostsList.js
+++ client/components/Admin/posts/PostsList.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { Flex } from "rebass";
 import PostItem from "./PostItem";
@@ -31,7 +32,14 @@ const PostsList = ({ posts }) => {
   console.log(posts);
   return (
     <div className=" w-full h-full max-h-full overflow-y-scroll p-4">
-      <h1 className="text-4xl">Posts</h1>
+      <div className="flex items-center justify-between ">
+        <h1 className="text-4xl">Posts</h1>
+        <Link href="/admin/posts/create">
+          <p className=" w-40 text-lg cursor-pointer py-3 flex items-center justify-center bg-green-100 hover:bg-green-200 rounded-lg font-medium uppercase tracking-wider text-green-700">
+            New Post
+          </p>
+        </Link>
+      </div>
       <div className="border mt-4 rounded-md shadow-xl">
         <div className="grid grid-cols-12 w-full gap-2 bg-blue-100 p-2 px-4  rounded-t-md ">
           {TITLES.map((t) => (
