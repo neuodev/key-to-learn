5a662e2f3849313c7c6198114f6da8e1eb3cab70
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-30 15:51:06 +0200

    Add comments screen

---
+++ admin/pages/comments.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const comments = () => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center text-4xl">
+      Comming Soon
+    </div>
+  );
+};
+
+export default comments;
