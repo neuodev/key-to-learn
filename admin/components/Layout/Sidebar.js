ac359ca8bdc6ce597473206b5b959620a61c6c8b
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-30 02:58:46 +0200

    Add logout button

--- admin/components/Layout/Sidebar.js
+++ admin/components/Layout/Sidebar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import { useSelector, useDispatch } from "react-redux";
+import { USER_SIGNIN_RESET } from "../../actions/constants";
 
 const MENU_ITEMS = [
   {
@@ -17,18 +19,25 @@ const MENU_ITEMS = [
 ];
 
 const SideBar = ({}) => {
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch({
+      type: USER_SIGNIN_RESET,
+    });
+  };
   return (
     <div
       id="admin-page-sidebar"
       className={`w-64 shadow-xl bg-white h-screen border-r`}
     >
       <div className="w-full h-full py-4">
-        <div className="my-4">
-          <ul>
+        <div className="py-4 flex justify-between flex-col w-full h-full ">
+          <ul className="px-2">
             {MENU_ITEMS.map((tab) => (
               <li
                 key={tab.title}
-                className="mb-1 font-thin text-lg text-left px-2 "
+                className="mb-1 font-thin text-lg text-left px-2 hover:bg-gray-200 rounded-md "
               >
                 <Link href={tab.path}>
                   <p
@@ -40,6 +49,14 @@ const SideBar = ({}) => {
               </li>
             ))}
           </ul>
+          <div className="px-2  justify-self-end align-bottom ">
+            <button
+              onClick={logout}
+              className="bg-red-200 text-red-500 font-medium uppercase tracking-wider py-3 px-4 rounded-md w-full text-left"
+            >
+              Logout
+            </button>
+          </div>
         </div>
       </div>
     </div>
